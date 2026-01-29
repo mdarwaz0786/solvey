5639e188de3bccb279b6ee80a50fe6a81cd89ad5
Md Arwaz <mdarwaznew2017@gmail.com>
2026-01-29 18:05:25 +0530

    change banner

--- client/src/pages/Home/SliderSection.jsx
+++ client/src/pages/Home/SliderSection.jsx
@@ -6,8 +6,8 @@ const SliderSection = () => {
 
   const slides = [
     { id: 1, image: "/solvey/newhomebanner0.png", link: "/" },
-    { id: 2, image: "/solvey/newhomebanner1.jpg", link: "/" },
-    { id: 3, image: "/solvey/newhomebanner2.jpg", link: "/" },
+    // { id: 2, image: "/solvey/newhomebanner1.jpg", link: "/" },
+    // { id: 3, image: "/solvey/newhomebanner2.jpg", link: "/" },
   ];
 
   useEffect(() => {
