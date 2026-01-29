578775cd994358c2160dd000f9bc266a4958269d
Md Arwaz <mdarwaznew2017@gmail.com>
2026-01-29 18:56:57 +0530

    make reponsive

--- client/src/pages/Home/SliderSection.jsx
+++ client/src/pages/Home/SliderSection.jsx
@@ -1,13 +1,12 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const SliderSection = () => {
   const [active, setActive] = useState(0);
 
   const slides = [
     { id: 1, image: "/solvey/newhomebanner0.png", link: "/" },
-    // { id: 2, image: "/solvey/newhomebanner1.jpg", link: "/" },
-    // { id: 3, image: "/solvey/newhomebanner2.jpg", link: "/" },
   ];
 
   useEffect(() => {
@@ -19,39 +18,40 @@ const SliderSection = () => {
   }, []);
 
   return (
-    <section
-      className="position-relative overflow-hidden"
-      style={{ height: "70vh", minHeight: "400px" }}
-    >
-      {slides.map((slide, index) => (
-        <div
-          key={slide.id}
-          className={`position-absolute top-0 start-0 w-100 h-100 
-            ${index === active ? "opacity-100" : "opacity-0"}`}
-          style={{ transition: "opacity 0.8s ease-in-out" }}
-        >
-          <a href={slide.link} className="d-block w-100 h-100">
-            <img
-              src={slide.image}
-              alt="slide"
-              className="w-100 h-100 img-fluid object-fit-cover"
-            />
-          </a>
-        </div>
-      ))}
+    <div className="position-relative overflow-hidden">
+      {/* Responsive Aspect Ratio Wrapper */}
+      <div className="ratio ratio-21x9 ratio-md-16x9 ratio-sm-4x3">
+        {slides.map((slide, index) => (
+          <div
+            key={slide.id}
+            className={`position-absolute top-0 start-0 w-100 h-100
+              ${index === active ? "opacity-100" : "opacity-0"}
+              transition-opacity`}
+          >
+            <Link to={slide.link} className="d-block w-100 h-100">
+              <img
+                src={slide.image}
+                alt="slide"
+                className="w-100 h-100 img-fluid object-fit-cover"
+              />
+            </Link>
+          </div>
+        ))}
+      </div>
 
-      <div className="position-absolute bottom-0 start-50 translate-middle-x mb-3 d-flex gap-2 z-3">
+      {/* Dots */}
+      {/* <div className="position-absolute bottom-0 start-50 translate-middle-x mb-3 d-flex gap-2">
         {slides.map((_, i) => (
           <button
             key={i}
             type="button"
-            className={`rounded-circle border-0 ${i === active ? "bg-primary" : "bg-white opacity-75"}`}
-            style={{ width: "15px", height: "15px" }}
             onClick={() => setActive(i)}
+            className={`rounded-circle border-0 p-2 
+              ${i === active ? "bg-primary" : "bg-white opacity-75"}`}
           />
         ))}
-      </div>
-    </section>
+      </div> */}
+    </div>
   );
 };
 
