045aad22b400399f78cf5a3fee5a6813bb1c936f
Md Arwaz <mdarwaznew2017@gmail.com>
2026-01-30 15:04:31 +0530

    change port

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     host: true,
     proxy: {
-      "/api": "http://localhost:8080",
+      "/api": "http://145.223.18.56:3008",
     },
   },
   plugins: [react()],
